d19f89d2c2a1f2f959e6a0fd1ca5163a2c4e872e
JesusAbarcaRodriguez <121053502+JesusAbarcaRodriguez@users.noreply.github.com>
2024-08-16 22:36:12 -0600

    [IMPLEMENTANDO CIFRADO CESAR]

--- controller/view_controller.py
+++ controller/view_controller.py
@@ -1,6 +1,36 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow
+import random
 class MainView(QMainWindow):
     def __init__(self):  # this
         super(MainView, self).__init__()
         uic.loadUi("view/view.ui", self)
+        self.encryptButton.clicked.connect(self.encrypt_text)
+
+    def encrypt_text(self):
+        text_to_encrypt = self.textToEncrypt.toPlainText()
+
+
+        key_input = self.keyInput.toPlainText()
+
+
+        cipher_type = self.cipherType.currentText()
+        if cipher_type == "Cifrado César":
+           self.generate_custom_alphabet(key_input, text_to_encrypt)
+        elif cipher_type == "Cifrado Vigenère":
+            print("Cifrado Vigenère")
+        # Mostrar el texto cifrado en el campo de salida
+        
+
+    def generate_custom_alphabet(self, key, text):
+        alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+
+        key = ''.join(sorted(set(key), key=key.index))
+
+        for char in key:
+            alphabet = alphabet.replace(char, "")
+
+        index_random = random.randint(0, len(alphabet))
+
+        custom_alphabet = alphabet[:index_random] + key + alphabet[index_random:]
+        self.decryptedText.setPlainText(custom_alphabet)
