9bbfd9e4a8683395d323744ecc4b616396dc9b19
JesusAbarcaRodriguez <121053502+JesusAbarcaRodriguez@users.noreply.github.com>
2024-08-16 22:51:52 -0600

    [CIFRADO POR CESAR IMPLEMENTADO]
    Falta desifrar y cifrar por el otro algoritmo (Vigenère)

--- controller/view_controller.py
+++ controller/view_controller.py
@@ -24,7 +24,7 @@ class MainView(QMainWindow):
 
     def generate_custom_alphabet(self, key, text):
         alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
-
+        alphabet_aux = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
         key = ''.join(sorted(set(key), key=key.index))
 
         for char in key:
@@ -33,4 +33,14 @@ class MainView(QMainWindow):
         index_random = random.randint(0, len(alphabet))
 
         custom_alphabet = alphabet[:index_random] + key + alphabet[index_random:]
-        self.decryptedText.setPlainText(custom_alphabet)
+
+        encrypted_text = ""
+        for char in text:
+            if char in alphabet_aux:
+                pos = alphabet_aux.index(char)
+                encrypted_text += custom_alphabet[pos]
+            else:
+                encrypted_text += char
+
+        self.decryptedText.setPlainText(alphabet_aux + "\n" +custom_alphabet + "\n" +encrypted_text)
+
